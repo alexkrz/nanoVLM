7ea573a049859737f3fb3be20ccb970e285a4a9e
Alexander Kurz <alex.kurz95@gmail.com>
2025-06-24 15:04:10 +0200

    Local adjustments

--- models/config.py
+++ models/config.py
@@ -44,7 +44,7 @@ class TrainConfig:
     lr_backbones: float = 1e-4
     data_cutoff_idx: int = None
     val_ratio: float = 0.01
-    batch_size: int = 256
+    batch_size: int = 16
     mmstar_batch_size: int = 32
     eval_in_epochs: bool = True
     epochs: int = 5
@@ -54,4 +54,4 @@ class TrainConfig:
     train_dataset_name: tuple[str, ...] = ("ai2d", "aokvqa", "chart2text", "chartqa", "clevr", "cocoqa", "datikz", "diagram_image_to_text", "docvqa", "dvqa", "figureqa", "finqa", "geomverse", "hateful_memes", "hitab", "iam", "iconqa", "infographic_vqa", "intergps", "localized_narratives", "mapqa", "multihiertt", "ocrvqa", "plotqa", "raven", "rendered_text", "robut_sqa", "robut_wikisql", "robut_wtq", "scienceqa", "screen2words", "st_vqa", "tabmwp", "tallyqa", "tat_qa", "textcaps", "textvqa", "tqa", "vistext", "visual7w", "visualmrc", "vqarad", "vqav2", "vsr", "websight") # "clevr_math", "okvqa", "spot_the_diff", "nlvr2", "mimic_cgd",
     test_dataset_path: str = "Lin-Chen/MMStar"
     wandb_entity: str = "HuggingFace" # Indicate the entity to log to in wandb
-    log_wandb: bool = True
+    log_wandb: bool = False
